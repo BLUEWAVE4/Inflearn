c26b865cc84ab80004b342133a78bf874007bc48
BLUEWAVE4 <105584381+BLUEWAVE4@users.noreply.github.com>
2022-05-20 00:41:54 +0900

    Add files via upload

--- chapter05_01_Function.py
+++ chapter05_01_Function.py
@@ -1,21 +1,20 @@
-# 프로그램이 발전하면서 코드의 복잡도가 상승(권한, 값 광범위)
-# 코드의 재사용성
-# ** 코드의 안정성 상승
+# 파이썬 함수 및 중요성
+# 파이썬 함수식 및 람다(Lambda)
 
 # 함수 정의 방법
-# def function_name(parameter): *parameter: 한도
+# def function_name(parameter):
 #   code
 
 # 예제1
-def first_func(w1):
-    print("Hello, ", w1)
+def first_func(w):
+    print("Hello, ", w)
 
 word = "Goodboy"
 
 first_func(word)
 
-# 예제 2
-def return_func(w1): #파이썬은 함수형 프로그래밍이다.
+# 예제2
+def return_func(w1):
     value = "Hello, " + str(w1)
     return value
 
@@ -23,7 +22,6 @@ x = return_func('Goodboy2')
 print(x)
 
 # 예제3(다중반환)
-
 def func_mul(x):
     y1 = x * 10
     y2 = x * 20
@@ -31,7 +29,6 @@ def func_mul(x):
     return y1, y2, y3
 
 x, y, z = func_mul(10)
-
 print(x, y, z)
 
 # 튜플 리턴
@@ -45,23 +42,11 @@ q = func_mul2(20)
 
 print(type(q), q, list(q))
 
-# 딕셔너리 리턴
-def func_mul2(x):
-    y1 = x * 10
-    y2 = x * 20
-    y3 = x * 30
-    return {'v1':y1, 'v2': y2, 'v3': y3}
-
-d = func_mul2(30)
-
-print(type(d), d, d.get('v2'), d.items(), d.keys())
-print()
-
 # 중요
 # *args, **kwargs
 
 # *args(언팩킹)
-def args_func(*args): # 매개변수 명 자유
+def args_func(*args): # 매개변수 몇 자유
     for i, v in enumerate(args):
         print('Result : {}'.format(i), v)
     print('-----')
@@ -71,60 +56,58 @@ args_func('Lee', 'Park')
 args_func('Lee', 'Park', 'Kim')
 
 # **kwargs(언팩킹)
-def kwargs_func(**kwargs): # 매개변수 명 자유
+def kwargs_func(**kwargs): # 매개변수 몇 자유
     for v in kwargs.keys():
         print("{}".format(v), kwargs[v])
-    print('-----')
+    print('------')
 
 kwargs_func(name1='Lee')
 
 kwargs_func(name1='Lee', name2='Park')
 
-# 전체 혼합
+# 전체혼합
 def example(args_1, args_2, *args, **kwargs):
     print(args_1, args_2, args, kwargs)
 
 example(10, 20, 'Lee', 'Kim', 'Park', age1=20, age2=30, age3=40)
 
 # 중첩함수
-
 def nested_func(num):
     def func_in_func(num):
         print(num)
     print("In func")
-    func_in_func(num+100)
+    func_in_func(num + 100)
 
 nested_func(100)
-print()
-#실행불가
-#func_in_func(1000)
+
+# 실행불가
+# func_in_func(1000)
 
 # 람다식 예제
 # 메모리 절약, 가독성 향상, 코드 간결
 # 함수는 객체 생성 -> 리소스(메모리) 할당
-# 람다는 즉시 실행 함수(Heap 초기화) -> 메모리 초기화 (메모리 효율적 사용)
+# 람다는 즉시 실행 함수(Heap 초기화) -> 메모리 초기화
 # 남발 시 가독성 오히려 감소
 
 #def mul_func(x, y):
-#    return x * y
+#    preturn x * y
 
-#lambda x, y: x * y
+#a = lambda x, y: x * y
 
-# 일반적 함수 -> 변
+# 일반적함수 -> 변수
 def mul_func(x, y):
     return x * y
 
-q = mul_func(10, 50)
-print(q)
+print(mul_func(10, 50))
 
 mul_func_var = mul_func
 print(mul_func_var(20, 50))
 
 # 람다 함수 -> 할당
-lambda_mul_func = lambda x,y : x * y
+lambda_mul_func = lambda x,y : x*y
 print(lambda_mul_func(50, 50))
 
 def func_final(x, y, func):
     print(x * y * func(100, 100))
 
-    func_final(10, 20, lambda x,y : x * y)
+func_final(10, 20, lambda x,y:x*y) # (10, 20, lambda_mul_func) 사용가능
