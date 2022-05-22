fd4f78666282158c809139aecfa77a99c93e0b47
BLUEWAVE4 <105584381+BLUEWAVE4@users.noreply.github.com>
2022-05-23 01:31:39 +0900

    Add files via upload

--- chapter06_03_Package.py
+++ chapter06_03_Package.py
@@ -1,8 +1,9 @@
 # 파이썬 패키지
 # 패키지 작성 및 사용법
 # 파이썬은 패키지로 분할 된 개별적인 모듈로 구성
+# __init__.py : Python 3.3 부터는 없어도 패키지로 인식 -> 단, 하위 호환을 위해 작성 추천
 # 상대경로 : ..(부모 디렉토리), .(현재 디렉토리) -> 모듈 내부에서만 사용
-# __init__.py : Python 3.3 부터는 없어도 패키지로 인식 -> 단, 하위 호환을 위해 작성 추가
+
 # 예제1
 import sub.sub1.module1
 import sub.sub2.module2
@@ -28,8 +29,12 @@ module1.mod1_test2()
 m2.mod2_test1()
 m2.mod2_test2()
 
+print()
+print()
+print()
+
 # 예제3
-from sub.sub1 import * #사용하지 않는 파일도 불러들여서, 불필요한 메모리를 사용할 수 있다.
+from sub.sub1 import *
 from sub.sub2 import *
 
 module1.mod1_test1()
@@ -37,12 +42,3 @@ module1.mod1_test2()
 
 module2.mod2_test1()
 module2.mod2_test2()
-
-# 예제4
-from sub.sub3 import *
-
-module3.mod3_test1()
-module3.mod3_test2()
-module3.mod3_test3()
-module3.mod3_test4()
-module3.mod3_test5()
